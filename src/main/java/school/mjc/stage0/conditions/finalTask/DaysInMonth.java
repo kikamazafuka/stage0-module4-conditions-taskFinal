90e3386170ea115742b728f80d6cc43de206bac3
Artur Semenas <uncelfuka@gmail.com>
2023-11-10 12:44:25 +0100

    Module #4.1. Final Task

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,39 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
+        if (year<0 || month<1 || month>12){
+            System.out.println("invalid date");
+        }else if (isLeap(year)&&month==2){
+            System.out.println(29);
+        } else amountOfDays(month);
+    }
 
+    private static boolean isLeap(int year){
+        return year%400==0 || year%4==0 && year%100!=0;
+    }
+
+    private static void amountOfDays(int month) {
+        switch (month){
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                System.out.println(30);
+                break;
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                System.out.println(31);
+                break;
+            case 2:
+                System.out.println(28);
+                break;
+            default:
+                System.out.println("wrong number!");
+        }
     }
 }
